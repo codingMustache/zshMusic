81361d2636928c09371b64172720ff21337848c9
codingMustache <jorge@codingMustache.dev>
2023-10-09 10:19:25 -0500

    fixed bug with dir

--- main.zsh
+++ main.zsh
@@ -1,38 +1,39 @@
 #!/bin/zsh
-
+dir="$(dirname "$0")"
+echo $dir
 music() {
   if [[ -z $1 ]]; then
     help
   else
     case $1 in
       -open | -o)
-          osascript ./appleScripts/open.applescript
+          osascript $dir/appleScripts/open.applescript
         ;;
       -artist | -a)
         if [[ -z $2 ]]; then
           echo "You Need to pass an Artist"
         else
-          osascript ./appleScripts/playArtist.applescript "$2"
+          osascript $dir/appleScripts/playArtist.applescript "$2"
         fi
         ;;
       -vol | -v)
         if [[ -z $2 ]]; then
           echo "This set volume 0-100"
         else
-          osascript ./appleScripts/volume.applescript "$2"
+          osascript $dir/appleScripts/volume.applescript "$2"
         fi
         ;;
       -stop | -s)
-        osascript ./appleScripts/pause.applescript "$2"
+        osascript $dir/appleScripts/pause.applescript "$2"
         ;;
       -play | -p)
-        osascript ./appleScripts/play.applescript "$2"
+        osascript $dir/appleScripts/play.applescript "$2"
         ;;
       -next | -n)
-        osascript ./appleScripts/next.applescript "$2"
+        osascript $dir/appleScripts/next.applescript "$2"
         ;;
       -current | -c)
-        osascript ./appleScripts/current.applescript "$2"
+        osascript $dir/appleScripts/current.applescript "$2"
         ;;
       *)
         help
